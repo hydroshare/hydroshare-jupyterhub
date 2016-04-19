c4edf1b71a2ce8433ff6588786eb8765c937975e
Tony Castronova <castronova.anthony@gmail.com>
2016-04-19 12:09:03 -0600

    testing arguments file

---
+++ arguments.py
@@ -0,0 +1,20 @@
+
+from tornado.web import RequestHandler
+
+
+def get_or_error(argname, strip=True):
+    """
+    This function gets a REST input argument or returns an error message if the argument is not found
+    Arguments:
+    argname -- the name of the argument to get
+    strip -- indicates if the whitespace will be stripped from the argument
+    """
+    
+    arg = RequestHandler.get_argument(argname, default=None, strip=strip)
+
+    if arg is None:
+        raise Exception('Could not find parameter "%s".  Please make sure that all required parameters have been provided' % argname)
+    
+    return arg
+    
+
--- handlers.py
+++ handlers.py
@@ -1,19 +1,23 @@
 import tornado.web
-
+import arguments
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        name = self.get_argument('name', default=None, strip=True)
-        if name is None:
-            self.write('missing parameter: "name"')
-            return
+
+        name = arguments.get_or_error('name', strip=True)
+#        name = self.get_argument('name', default=None, strip=True)
+#        if name is None:
+#            self.write('missing parameter: "name"')
+#            return
 
         self.write('Hello %s'%name)
 
 
 class JupyterHandler(tornado.web.RequestHandler):
     def get(self):
-        notebook = self.get_argument('notebook', default=None, strip=True)
+        username = self.get_argument('notebook', default=None, strip=True)
+        resourcetype = self.get_argument('notebook', default=None, strip=True)
+        resourceid = self.get_argument('notebook', default=None, strip=True)
         if notebook is None:
             self.write('missing parameter: "notebook"')
             return
