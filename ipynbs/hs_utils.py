b113e92a7cbf88458696fc7befc506f6a550e83f
Tony Castronova <castronova.anthony@gmail.com>
2016-04-20 16:35:57 -0600

    added docstrings to hs_utils.pt

--- ipynbs/hs_utils.py
+++ ipynbs/hs_utils.py
@@ -5,6 +5,15 @@ from hs_restclient import HydroShare
 
 
 def getSecureConnection(username):
+    """
+    Establishes a secure connection with HydroShare.
+
+    Args:
+        username: HydroShare username
+    Returns:
+        HydroShare connection 
+    """
+    
     p = getpass.getpass('Enter you HydroShare Password: ')
     auth = HydroShareAuthBasic(username=username, password=p)
     hs = HydroShare(auth=auth)
@@ -12,6 +21,16 @@ def getSecureConnection(username):
 
 
 def getResourceContent(hs_connection, resourceid, destination='.'):
+    """
+    Downloads the content of HydroShare resource to the JupyterHub userspace
+
+    Args:
+        hs_connection: secure connection to hydroshare, HydroShare.HydroShare() object
+        resourceid: id of the HydroShare resource to query
+        destination: path relative to /user/[username]/notebooks/ipynbs/content
+
+    """
+    
     jupyter_username = getpass.getuser()
     ip = socket.gethostbyname(socket.gethostname())
     base_dir = 'http://%s/user/%s/tree/ipynbs/content' % (ip, jupyter_username)
