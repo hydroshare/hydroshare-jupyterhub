de453317ccd4831e7caa32915ab689039162f5d2
Tony Castronova <castronova.anthony@gmail.com>
2016-04-20 15:35:39 -0600

    added verbose console messaging to the tornado server

--- jupteryhub_server.py
+++ jupteryhub_server.py
@@ -2,8 +2,11 @@ from tornado import httpserver
 from tornado import gen
 from tornado.ioloop import IOLoop
 import tornado.web
+from tornado.log import enable_pretty_logging
 import handlers as resthandlers
+import socket
 
+enable_pretty_logging()
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -11,11 +14,12 @@ class Application(tornado.web.Application):
             (r"/?", resthandlers.MainHandler),
             (r"/jupyter/?",resthandlers.JupyterHandler)
         ]
-        tornado.web.Application.__init__(self, handlers, debug=True)
+        tornado.web.Application.__init__(self, handlers, debug=True, log_function=True)
 
 def main():
     app = Application()
     app.listen(8080)
+    print('JupyterHub REST server listening on %s:8080' % socket.gethostbyname(socket.gethostname())
     IOLoop.instance().start()
 
 if __name__ == '__main__':
