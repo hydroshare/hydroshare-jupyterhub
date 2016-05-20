0cdf08d1ba3c00bb4da0e764e31da7efce19e487
tony castronova <castro@jupyterhub.bluezone>
2016-05-20 12:05:29 -0600

    fixed hs_utils basic auth, added taudem example, add readme files to data and documents folders

--- notebooks/utilities/hs_utils.py
+++ notebooks/utilities/hs_utils.py
@@ -1,42 +1,50 @@
 import os
 import getpass
 import socket
-from hs_restclient import HydroShare, HydroShareAuthBasic
+from hs_restclient import HydroShare, HydroShareAuthBasic, HydroShareHTTPException
+
+class hydroshare():
+    def __init__(self):
+        self.hs = None
+
+    def getSecureConnection(self, email):
+        """
+        Establishes a secure connection with HydroShare.
+
+        Args:
+            email: email address associated with HydroShare
+        Returns:
+            HydroShare connection 
+        """
+
+        p = getpass.getpass('Enter you HydroShare Password: ')
+        auth = HydroShareAuthBasic(username=email, password=p)
+        self.hs = HydroShare(auth=auth)
+        
+        try:
+            self.hs.getUserInfo()
+            print('Successfully established a connection with HydroShare')
+        except HydroShareHTTPException as e:
+            print(e)
+            self.hs = None
 
 
-def getSecureConnection(username):
-    """
-    Establishes a secure connection with HydroShare.
+    def getResourceContent(self,resourceid, destination='.'):
+        """
+        Downloads the content of HydroShare resource to the JupyterHub userspace
 
-    Args:
-        username: HydroShare username
-    Returns:
-        HydroShare connection 
-    """
-    
-    p = getpass.getpass('Enter you HydroShare Password: ')
-    auth = HydroShareAuthBasic(username=username, password=p)
-    hs = HydroShare(auth=auth)
-    return hs
+        Args:
+            resourceid: id of the HydroShare resource to query
+            destination: path relative to /user/[username]/notebooks/data
 
+        """
 
-def getResourceContent(hs_connection, resourceid, destination='.'):
-    """
-    Downloads the content of HydroShare resource to the JupyterHub userspace
-
-    Args:
-        hs_connection: secure connection to hydroshare, HydroShare.HydroShare() object
-        resourceid: id of the HydroShare resource to query
-        destination: path relative to /user/[username]/notebooks/ipynbs/content
-
-    """
-    
-    jupyter_username = getpass.getuser()
-    ip = socket.gethostbyname(socket.gethostname())
-    base_dir = 'http://%s/user/%s/tree/ipynbs/content' % (ip, jupyter_username)
-    dst = os.path.join(base_dir, destination)
-    getattr(hs_connection,'getResource')(resourceid, destination=dst, unzip=True)
-    return dst
+        jupyter_username = getpass.getuser()
+        ip = socket.gethostbyname(socket.gethostname())
+        base_dir = 'http://%s/user/%s/tree/notebooks/data/' % (ip, jupyter_username)
+        dst = os.path.join(base_dir, destination)
+        self.hs.getResource(resourceid, destination=dst, unzip=True)
+        return dst
 
 
 
