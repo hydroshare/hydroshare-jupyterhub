68eab03fef0f6c7f387cf0e3a67088f31700b0c3
tony castronova <castro@jupyterhub.bluezone>
2016-05-23 12:23:36 -0600

    cleaned jupyterhub config

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,11 +1,8 @@
 # Configuration file for Jupyter Hub
-
-
 c = get_config()
 
 # spawn with Docker
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-#c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
 c.JupyterHub.confirm_no_ssl = True
 c.JupyterHub.port = 80
 c.DockerSpawner.hub_ip_connect = '129.123.51.34'
@@ -13,33 +10,9 @@ c.JupyterHub.hub_ip = '129.123.51.34'
 c.JupyterHub.extra_log_file = './jupyterhub.log'
 
 
-# OAuth with GitHub
+# OAuth with HydroShare
 c.JupyterHub.authenticator_class = 'oauthenticator.HydroShareOAuthenticator'
-#c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
-
-c.Authenticator.whitelist = whitelist = set()
-c.Authenticator.admin_users = set()
-#c.Authenticator.admin_users = admin = set()
-
-import os
-
-join = os.path.join
-here = os.path.dirname(__file__)
-with open(join(here, 'userlist')) as f:
-    for line in f:
-        if not line:
-            continue
-        parts = line.split()
-        name = parts[0]
-        whitelist.add(name)
-        if len(parts) > 1 and parts[1] == 'admin':
-            admin.add(name)
-
 c.HydroShareOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-#c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-
-#c.Spawner.notebook_dir = '/home/tony'
-#c.Spawner.args = ['--NotebookApp.default_url=/notebooks/Welcome.ipynb']
 
 # mount the userspace directory
 c.DockerSpawner.volumes = {
