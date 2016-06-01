cdacb03561a600ab2109b0cd7fa386d32ab837b1
tony castronova <castro@jupyterhub.bluezone>
2016-06-01 12:11:08 -0600

    fixed cleanup.sh

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo Running Cleanup
-docker ps -a | grep 'Up' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+docker ps -a | grep 'Up [0-9] days' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 echo Finished
 
 
