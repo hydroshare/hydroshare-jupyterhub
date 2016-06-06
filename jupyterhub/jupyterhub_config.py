48de84ac23b5e70fd4a8abd036312a178ab5b089
tony castronova <castro@jupyterhub.bluezone>
2016-06-06 11:27:08 -0600

    referencing jupyterhub config properties from environment variables

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -3,13 +3,22 @@ import os
 # Configuration file for Jupyter Hub
 c = get_config()
 
-# spawn with Docker
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.JupyterHub.confirm_no_ssl = True
-c.JupyterHub.port = 80
-c.DockerSpawner.hub_ip_connect = '129.123.51.34'
-c.JupyterHub.hub_ip = '129.123.51.34'
-c.JupyterHub.extra_log_file = './jupyterhub.log'
+try:
+    # spawn with Docker
+    c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+    c.JupyterHub.confirm_no_ssl = True
+    c.JupyterHub.port = int(os.environ['JUPYTER_PORT'])
+    c.DockerSpawner.hub_ip_connect = os.environ['JUPYTER_IP']
+    c.JupyterHub.hub_ip = os.environ['JUPYTER_IP']
+    c.JupyterHub.extra_log_file = os.environ['JUPYTER_LOG']
+    userspace = os.path.join(os.environ['JUPYTER_USERSPACE_DIR'], '{username}')
+except Exception as e:
+    print('Error setting JupyterHub settings from environment variables.  Please make sure that the following environment variables are set properly in ./env:\n  JUPYER_PORT\n  JUPYTER_IP\n  JUPYTER_LOG\n  JUPYTER_USERSPACE_DIR\n\n%s' % e)
+
+#c.JupyterHub.port = 80
+#c.DockerSpawner.hub_ip_connect = '129.123.51.34'
+#c.JupyterHub.hub_ip = '129.123.51.34'
+#c.JupyterHub.extra_log_file = './jupyterhub.log'
 
 
 # OAuth with HydroShare
@@ -17,8 +26,10 @@ c.JupyterHub.authenticator_class = 'oauthenticator.HydroShareOAuthenticator'
 c.HydroShareOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 
 # mount the userspace directory
+print('USERSPACE: ',userspace)
 c.DockerSpawner.volumes = {
-    '/home/castro/userspace/{username}': '/home/jovyan/work',
+    userspace: '/home/jovyan/work',
+#    '/home/castro/userspace/{username}': '/home/jovyan/work',
 }
 
 
