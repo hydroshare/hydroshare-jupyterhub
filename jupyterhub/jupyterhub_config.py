cab1a9d59b1b5f66cc13bdfd4d0b10617b039161
Castronova <castronova.anthony@gmail.com>
2016-07-13 10:28:13 -0600

    added custom css and javascript to notebooks.  Added username to header bar

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,4 +1,5 @@
 import os
+from os.path import *
 
 # Configuration file for Jupyter Hub
 c = get_config()
@@ -19,11 +20,22 @@ except Exception as e:
 c.JupyterHub.authenticator_class = 'oauthenticator.HydroShareOAuthenticator'
 c.HydroShareOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 
+#c.extra_static_paths = ['../static/javascript/custom.js']
+#js = abspath(join(basename(__file__), '../../static/js'))
+#css = abspath(join(basename(__file__), '../../static/css'))
+static = abspath(join(basename(__file__), '../../static/custom'))
+
+#print(js, ' Path Exists: ', exists(js))
+#print(css, ' Path Exists: ', exists(css))
+print(static, ' Path Exists: ', exists(static))
+
 # mount the userspace directory
 print('USERSPACE: ',userspace)
 print('MOUNTING USERSPACE: %s -> /home/jovyan/work' % userspace)
 c.DockerSpawner.volumes = {
     userspace: '/home/jovyan/work',
+    static: '/home/jovyan/work/notebooks/.jupyter/custom',
 }
 
+#c.NotebookApp.extra_static_paths = ['/home/jovyan/work/notebooks/.ipython/profile_default/static']
 
