0d91cc53871bf30aa4ec0a52d899852303ea454e
Castronova <castronova.anthony@gmail.com>
2017-12-01 11:25:09 -0500

    #95 updates to config.py to address version changes

--- jupyterhub/config.py
+++ jupyterhub/config.py
@@ -6,18 +6,28 @@ import sys
 c = get_config()
 
 # set container culling properties
-c.JupyterHub.services = [
-    {
-        'name': 'cull-idle',
-        'admin': True,
-        'command': 'python3 /etc/jupyterhub/cull/cull_idle_servers.py --timeout=3600'.split(),
-    }
-]
+#c.JupyterHub.services = [
+#    {
+#        'name': 'cull-idle',
+#        'admin': True,
+#        'command': 'python3 /etc/jupyterhub/cull/cull_idle_servers.py --timeout=3600'.split(),
+#    }
+#]
+
+ssl_dir = '/etc/ssl/certs/cuahsi.org'
 
 try:
     # spawn with Docker
     c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
     c.JupyterHub.confirm_no_ssl = True
+
+    c.DockerSpawner.extra_host_config = {'mem_limit':'5g'}
+
+    # https on :443
+#    c.JupyterHub.port = 443
+#    c.JupyterHub.ssl_key = join(ssl_dir, 'cuahsi.key')
+#    c.JupyterHub.ssl_cert = join(ssl_dir, 'cuahsi.cer')
+
     c.JupyterHub.port = int(os.environ['JUPYTER_PORT'])
     c.DockerSpawner.hub_ip_connect = os.environ['DOCKER_SPAWNER_IP']
     c.DockerSpawner.remove_containers = True
@@ -44,17 +54,15 @@ c.DockerSpawner.volumes = {
    os.environ['JUPYTER_STATIC_DIR']: '/home/jovyan/.jupyter/custom',
 }
 
-# SSL
-c.NotebookApp.certfile = u'/volume/hydro-develop/cert.pem'
-c.NotebookApp.keyfile = u'/volume/hydro-develop/key.pem'
 # IRODS settings 
 # http://stackoverflow.com/questions/37144357/link-containers-with-the-docker-python-api
-#c.DockerSpawner.extra_host_config = {
-#    'privileged':True,
-#    'cap_add':['SYS_ADMIN','MKNOD'],
-#    'devices':['/dev/fuse'],
-#    'security_opt':['apparmor:unconfined']
-#}
+c.DockerSpawner.extra_host_config = {
+    'privileged':True,
+    'cap_add':['SYS_ADMIN','MKNOD'],
+    'devices':['/dev/fuse'],
+    'security_opt':['apparmor:unconfined'],
+    'mem_limit':'5g'
+}
 
-#c.NotebookApp.extra_static_paths = ['/home/jovyan/work/notebooks/.ipython/profile_default/static']
-
+c.NotebookApp.extra_static_paths = ['/home/jovyan/work/notebooks/.ipython/profile_default/static']
+c.DockerSpawner.notebook_dir = '/home/jovyan/work'
