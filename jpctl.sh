31969505d200d282847077db149ff9ffcc8564fb
Castronova <castronova.anthony@gmail.com>
2017-12-04 16:44:18 -0500

    #73 removed deprecated functions from jpctl

--- jpctl.sh
+++ jpctl.sh
@@ -26,41 +26,8 @@ clean() {
   docker rmi $(docker images -q -f dangling=true) 2> /dev/null || true
   echo "done" 
 
-  # parse args if they are provided
-  if [[ $# -ne 0 ]] ;  then
-    if [[ $1 == "--systemd" ]]; then
-       clean_systemd
-    elif [[ $1 == "--screen" ]]; then
-       clean_screen
-    else
-       echo -e "--> [Error] invalid argument: $1\nSee help for vaild start arguments\n"
-    fi
-  else
-     echo -e "--> No clean argument provided, just cleaning docker containers and images"
-  fi
-}
-
-clean_screen(){
-  
-  echo -e "--------------------------------\n"
-  echo -e "--     DEPRECATED FUNCTION    --\n"
-  echo -e "--------------------------------\n"
-  
-  # remove error files
-  echo -n "--> removing screen error logs..."
-  sudo rm $LOG_PATH/*.err 2> /dev/null || true
-  sudo rm $JUPYTER_PATH/jupyter.log 2> /dev/null || true
-  echo "done"
-
-  # remove jupyterhub files
-  echo -n "--> removing screen database..."
-  sudo rm $JUPYTER_PATH/jupyter.sqlite 2> /dev/null || true
-  echo "done"
-
-  echo -n "--> removing screen cookies..."
-  sudo rm $JUPYTER_PATH/jupyterhub_cookie_secret 2> /dev/null || true
-  echo "done"
- 
+  # clean systemd cookies
+  clean_systemd
 }
 
 clean_systemd(){
@@ -81,20 +48,6 @@ clean_systemd(){
   echo -n "--> removing systemd cookies..."
   sudo rm /etc/jupyterhub/server/*cookie_secret 2> /dev/null || true
   echo "done"
-}
-
-install_base_ubuntu() {
-    # install jupyterhub dependencies
-    echo -e "--> installing system requirements"
-    sudo apt-get clean  
-    sudo apt-get update --fix-missing  
-    sudo apt-get install -y openssh-server wget screen docker python3-dateutil, tree
-
-    # install node and configurable proxy
-    echo -e "--> installing nodejs and configurable-http-proxy"
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-    sudo apt-get install -y nodejs 
-    sudo npm install -g configurable-http-proxy
 }
 
 install_base_rhel() {
@@ -122,20 +75,7 @@ install_base_rhel() {
 
 install() {
 
-    # parse args
-    if [[ $# -ne 0 ]] ;  then
-	if [[ $1 == "--ubuntu" ]]; then
-	    install_base_ubuntu
-	elif [[ $1 == "--rhel" ]]; then
-	    install_base_rhel
-        else
-	    echo -e "--> [Error] invalid installation platform argument: $1\nSee help for valid arguments\n"
-	    return -1
-	fi
-    else
-	echo -e "--> [Error] missing installation platform argument: \nSee help for valid arguments\n"
-	return -1
-    fi
+    install_base_rhel
 
     # activate and enable docker
     sudo systemctl enable docker
@@ -316,130 +256,9 @@ stop_services() {
 
 }
 
-stop_screen() {
-
-  echo -e "--------------------------------\n"
-  echo -e "--     DEPRECATED FUNCTION    --\n"
-  echo -e "--------------------------------\n"
-
-  echo "Shutting screen instances"
-
-  echo -n "--> killing rest..."
-  if sudo screen -list | grep -q "rest"; then
-    sudo screen -S rest -X at "#" stuff $'\003'
-    sudo screen -X -S rest quit > /dev/null
-    sleep 1
-  fi
-  echo "done"
-
-  echo -n "--> killing jupyter..."
-  if sudo screen -list | grep -q "jupyter"; then
-    sudo screen -S jupyter -X at "#" stuff $'\003'
-    sudo screen -X -S jupyter quit > /dev/null
-    sleep 1
-  fi
-  echo "done"
-
-  echo -n "--> killing collector..."
-  if sudo screen -list | grep -q "collector"; then
-    sudo screen -S collector -X at "#" stuff $'\003'
-    sudo screen -X -S collector quit > /dev/null
-    sleep 1
-  fi
-  echo "done"
-
-}
-
 start_services() {
 
-  # parse args if they are provided
-  if [[ $# -ne 0 ]] ;  then
-    if [[ $1 == "--screen" ]]; then
-        restart_screen
-    else
-       echo -e "invalid argument: $1\nSee help for vaild start arguments\n"
-    fi
-  else
-     start_systemctl
-  fi
-}
-
-restart_screen() {
-
-  echo -e "--------------------------------\n"
-  echo -e "--     DEPRECATED FUNCTION    --\n"
-  echo -e "--------------------------------\n"
-  
-  JUPYTER_CMD="start"
-  RUN_CULL=true
-  if [[ $# -ne 0 ]] ;  then
-    if [[ $1 == "--debug" ]]; then
-       JUPYTER_CMD="start-debug"
-       RUN_CULL=false
-    fi
-  fi
-
-  stop_services 
-  
-  # remove the error logs before attempting restart 
-  if ls | grep -q "rest.err"; then
-      sudo rm rest.err > /dev/null
-  fi
-
-  if ls | grep -q "jupyter.err"; then
-      sudo rm jupyter.err > /dev/null
-  fi
-
-  if ls | grep -q "cull.err"; then
-      sudo rm cull.err > /dev/null
-  fi
-
-  # make output dir
-  mkdir -p ./log
-
-  echo -e "\nRestarting screen instances"
-  echo -n "--> starting rest..."
-  sudo screen -dmS rest sh -c "cd $REST_PATH &&  ./run.sh >$LOG_PATH/rest.out 2> $LOG_PATH/rest.err"
-  sleep 1 # give the session time to spin up
-  if ! sudo screen -list | grep -q "rest"; then
-    echo -e "\n\n**********************"
-    echo -e "Failed to start rest"
-    echo "**********************"
-    cat $LOG_PATH/rest.err
-    return -1
-  fi
-  echo "done"
-
-  echo -n "--> starting jupyter..."
-  sudo screen -dmS jupyter sh -c "cd $JUPYTER_PATH && ./run.sh $JUPYTER_CMD > $LOG_PATH/jupyter.out 2> $LOG_PATH/jupyter.err"
-  sleep 1 # give the session time to spin up
-  if ! sudo screen -list | grep -q "jupyter"; then
-    echo -e "\n\n**********************"
-    echo -e "Failed to start jupyter"
-    echo "**********************"
-    cat $LOG_PATH/jupyter.err
-    return -1
-  fi
-  echo "done"
-  
-  if "$RUN_CULL" = true; then
-    echo -n "--> starting collector..."
-    sudo screen -dmS collector sh -c "cd $JUPYTER_PATH && ./run_cull.sh > $LOG_PATH/cull.out 2> $LOG_PATH/cull.err"
-    sleep 1 # give the session time to spin up
-    if ! sudo screen -list | grep -q "collector"; then
-      echo -e "\n\n**********************"
-      echo -e "Failed to start collector"
-      echo "**********************"
-      cat $LOG_PATH/cull.err
-      return -1
-    fi
-    echo "done"
-  else 
-    echo "--> skipping collector startup because you are running in debug mode" 
-  fi
-
-  echo -e "\nRunning Screens"
-  sudo screen -list 
+   start_systemctl
 }
 
 start_systemctl(){
@@ -502,20 +321,22 @@ restart_services(){
 
 display_usage() {
    echo "*** JupyterHub Control Script ***"
-   echo "usage: $0 install --ubuntu   # install required software and build jupyterhub docker containers (ubuntu)"
-   echo "usage: $0 install --rhel     # install required software and build jupyterhub docker containers (RHEL)"
-   echo "usage: $0 restart	      # restart the jupyterhub service" 
-   echo "usage: $0 configure	      # set/update jupyterhub configuration files"
-   echo "usage: $0 config-envs	      # set the JupyterHub environment variables"
+   echo ""
+   echo "usage: $0 install            # install required software and build jupyterhub docker containers (RHEL)"
+   echo "usage: $0 configure          # set/update jupyterhub configuration files"
+   echo "usage: $0 config-envs        # set the JupyterHub environment variables"
+   echo ""
+   echo "usage: $0 start              # start the jupyterhub services (using systemd)"
+   echo "usage: $0 stop               # stop all jupyterhub services (using systemd)"
+   echo "usage: $0 restart            # restart the jupyterhub service" 
+   echo ""
    echo "usage: $0 build              # build the jupyter docker images"
    echo "usage: $0 build --clean      # force a clean build the jupyter docker images"
    echo "usage: $0 update             # update the base docker image on a production server (designed to minimize server downtime)"
-   echo "usage: $0 start              # start the jupyterhub services (using systemd)"
-   echo "usage: $0 start --debug      # [DEPRECATED] start the jupyterhub in debug mode, necessary for development (using screen)"
-   echo "usage: $0 stop               # stop all jupyterhub services (using systemd)"
-   echo "usage: $0 clean --systemd    # clean all jupyterhub images, containers, and system files (using systemd)"
-   echo "usage: $0 clean --screen     # [DEPRECATED] clean all jupyterhub images, containers, and system files (using screen)"
+   echo "usage: $0 clean              # clean all jupyterhub images, containers, and system files (using systemd)"
+   echo ""
    echo "usage: $0 test               # run unittests"
+   echo ""
    echo "***"
 }
 
@@ -526,13 +347,13 @@ if [ $# -eq 0 ] ; then
 fi
 
 case "$1" in
-    install) install ${2:-}
+    install) install
         ;;
-    start) start_services ${2:-}
+    start) start_services
         ;;
-    stop) stop_services ${2:-}
+    stop) stop_services
         ;;
-    clean) clean ${2:-}
+    clean) clean
         ;;
     build) build_docker ${2:-}
         ;;
