46d7f40ebff7c014bea87df2f27910a422a3381e
Castronova <castronova.anthony@gmail.com>
2018-06-04 12:38:04 -0400

    added tree view for overwrite to avoid accidental data loss

--- notebooks/utilities/utils.py
+++ notebooks/utilities/utils.py
@@ -1,3 +1,4 @@
+import os
 import inspect
 import shutil
 import sys
@@ -59,3 +60,33 @@ class heartbeat(object):
         args += tuple([next(self.pulse)])
         sys.stdout.write(msg % args)
         sys.stdout.flush()
+        
+        
+def _realname(path, root=None):
+    if root is not None:
+        path=os.path.join(root, path)
+    result=os.path.basename(path)
+    if os.path.islink(path):
+        realpath=os.readlink(path)
+        result= '%s -> %s' % (os.path.basename(path), realpath)
+    return result
+
+def tree(startpath, depth=-1):
+    prefix=0
+    if startpath != '/':
+        if startpath.endswith('/'): startpath=startpath[:-1]
+        prefix=len(startpath)
+    for root, dirs, files in os.walk(startpath):
+        level = root[prefix:].count(os.sep)
+        if depth >-1 and level > depth: continue
+        indent=subindent =''
+        if level > 0:
+            indent = '|   ' * (level-1) + '|-- '
+        subindent = '|   ' * (level) + '|-- '
+        print('{}{}/'.format(indent, _realname(root)))
+        # print dir only if symbolic link; otherwise, will be printed as root
+        for d in dirs:
+            if os.path.islink(os.path.join(root, d)):
+                print('{}{}'.format(subindent, _realname(d, root=root)))
+        for f in files:
+            print('{}{}'.format(subindent, _realname(f, root=root)))
