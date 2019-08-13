b925648a04138ba67372f87f2e58e9d34fc698c5
Castronova <castronova.anthony@gmail.com>
2019-08-13 12:03:15 -0400

    modified file permission setting to speed up launch time

--- jupyterhub_rest_server/jupyterhub_rest_server/utilities.py
+++ jupyterhub_rest_server/jupyterhub_rest_server/utilities.py
@@ -1,6 +1,6 @@
 # this file contains utility functions used by the RequestHandlers
 
-import os, stat, sys
+import os, stat
 from pwd import getpwnam
 import grp
 import shutil
@@ -12,8 +12,9 @@ log = logging.getLogger()
 def set_hydroshare_args(username, resourceid, resourcetype):
 
     userspace_dir = os.environ['JUPYTER_USERSPACE_DIR']
-    hs_env = os.path.abspath(os.path.join(userspace_dir, '%s/.env' % username.lower()))
-    print('ENV_PATH ',hs_env, file=sys.stderr)
+    hs_env = os.path.abspath(os.path.join(userspace_dir, '%s/notebooks/.env' % username.lower()))
+    print('ENV_PATH ',hs_env)
+
     with open(hs_env, 'w') as f:
         f.write('HS_USR_NAME=%s\n' % username)
         f.write('HS_RES_ID=%s\n' % resourceid)
@@ -42,15 +43,13 @@ def build_userspace(username):
     ipynb_dir = os.environ['JUPYTER_NOTEBOOK_DIR']
 
     # check to see if user exists
-    basepath = os.path.abspath(os.path.join(userspace_dir, '%s' % husername))
-    # path = os.path.abspath(os.path.join(basepath, 'notebooks'))
-    path = os.path.join(basepath, 'notebooks')
+    basepath = os.path.abspath(os.path.join(userspace_dir, '%s'%husername))
+    path = os.path.abspath(os.path.join(basepath, 'notebooks'))
     if not os.path.exists(path):
         os.makedirs(path)
 
     file_paths = []
-    print('%s -> Copying userpace files' % username, flush=True)
-    #ipynb_dir = '../jupyter-rest-endpoint/notebooks'
+    print('%s -> copying userpace files' % username, flush=True)
     for root, dirs, files in os.walk(ipynb_dir):
         for file in files:
             file_paths.append(os.path.join(os.path.abspath(root), file))
@@ -61,21 +60,23 @@ def build_userspace(username):
         dirpath = os.path.dirname(dst)
         if not os.path.exists(dirpath):
             os.makedirs(dirpath)
-        print("Copying %s -> %s" % (src, dst))
+            os.chown(dirpath, uid, gid)
+            os.chmod(dirpath, 0o2770)
         shutil.copyfile(src, dst)
+        os.chown(dst, uid, gid)
+        os.chmod(dst, 0o2770)
 
-    # change file ownership so that it can be accessed inside docker container
-    print('%s -> modifying userspace permissions' % username, flush=True)
-    os.chown(basepath, uid, gid)
-    os.chmod(basepath, stat.S_IRWXG |  stat.S_IRWXU)
-    os.chown(os.path.dirname(basepath), uid, gid)
-    os.chmod(os.path.dirname(basepath), stat.S_IRWXG | stat.S_IRWXU)
-
-    for root, dirs, files in os.walk(basepath):
-        for d in dirs:
-            os.chown(os.path.join(root, d), uid, gid)
-            os.chmod(os.path.join(root, d), stat.S_IRWXG |  stat.S_IRWXU)
-
-        for f in files:
-            os.chown(os.path.join(root, f), uid, gid)
-            os.chmod(os.path.join(root, f), stat.S_IRWXG | stat.S_IRWXU)
+#    # change file ownership so that it can be accessed inside docker container
+#    print('%s -> modifying userspace permissions' % username, flush=True)
+#    os.chown(basepath, uid, gid)
+#    os.chown(os.path.dirname(basepath), uid, gid)
+#    os.chmod(os.path.dirname(basepath), 0o2770)
+#
+#    for root, dirs, files in os.walk(basepath):
+#        for d in dirs:
+#            os.chown(os.path.join(root, d), uid, gid)
+#            os.chmod(os.path.join(root, d), 0o2770)
+#
+#        for f in files:
+#            os.chown(os.path.join(root, f), uid, gid)
+#            os.chmod(os.path.join(root, f), 0o2770)
