3ec221acf2b1170265b7d82911f72b2b57e8ba67
Castronova <castronova.anthony@gmail.com>
2019-09-25 19:07:27 -0400

    more fixes to file permissions in userspace

--- jupyterhub_rest_server/jupyterhub_rest_server/utilities.py
+++ jupyterhub_rest_server/jupyterhub_rest_server/utilities.py
@@ -42,50 +42,58 @@ def build_userspace(username):
     userspace_dir = os.environ['JUPYTER_USERSPACE_DIR']
     ipynb_dir = os.environ['JUPYTER_NOTEBOOK_DIR']
 
-    # check to see if user exists
-    basepath = os.path.abspath(os.path.join(userspace_dir, '%s'%husername))
+    # set the path to the user's directory in the 
+    # userspace mount
+    basepath = os.path.abspath(os.path.join(userspace_dir,
+                                            '%s' % husername))
+    # define the path to the users notebooks
     path = os.path.abspath(os.path.join(basepath, 'notebooks'))
 
+    # prepare the user's root direcory
     # make the root directory if it doesn't exist
     if not os.path.exists(path):
         os.makedirs(path)
 
-        # set permissions
-        os.chown(path, uid, gid)
-        os.chmod(path, 0o2770)
+    # set permissions for the root path and
+    # the users notebooks path regardless of the
+    # directories already exist to make sure we don't
+    # run into permissions issues
+    os.chown(path, uid, gid)
+    os.chmod(path, 0o2770)
+    os.chown(basepath, uid, gid)
+    os.chmod(basepath, 0o2770)
 
-        os.chown(basepath, uid, gid)
-        os.chmod(basepath, 0o2770)
-
-    file_paths = []
+    # collect all the paths for the files that will be
+    # copied into the user's notebooks directory
     print('%s -> copying userpace files' % username, flush=True)
+    file_paths = []
     for root, dirs, files in os.walk(ipynb_dir):
         for file in files:
             file_paths.append(os.path.join(os.path.abspath(root), file))
+
+    # generate list of relative paths that will be used to copy
+    # the default files into the user's notebook directory
     relpaths = [os.path.relpath(p, ipynb_dir) for p in file_paths]
+
+    # loop through each of the file paths and add them to the
+    # user's userspace directory. Make sure permissions are set
+    # correctly.
     for i in range(0, len(file_paths)):
         src = file_paths[i]
         dst = os.path.join(path, relpaths[i])
         dirpath = os.path.dirname(dst)
+
+        # only create the dir of it doesn't already exist
         if not os.path.exists(dirpath):
             os.makedirs(dirpath)
-            os.chown(dirpath, uid, gid)
-            os.chmod(dirpath, 0o2770)
+
+        # always set the permission for folders whether or not
+        # they already exist to make sure we don't run into
+        # permissions issues
+        os.chown(dirpath, uid, gid)
+        os.chmod(dirpath, 0o2770)
+
+        # copy the file and set permissions
         shutil.copyfile(src, dst)
         os.chown(dst, uid, gid)
         os.chmod(dst, 0o2770)
-
-#    # change file ownership so that it can be accessed inside docker container
-#    print('%s -> modifying userspace permissions' % username, flush=True)
-#    os.chown(basepath, uid, gid)
-#    os.chown(os.path.dirname(basepath), uid, gid)
-#    os.chmod(os.path.dirname(basepath), 0o2770)
-#
-#    for root, dirs, files in os.walk(basepath):
-#        for d in dirs:
-#            os.chown(os.path.join(root, d), uid, gid)
-#            os.chmod(os.path.join(root, d), 0o2770)
-#
-#        for f in files:
-#            os.chown(os.path.join(root, f), uid, gid)
-#            os.chmod(os.path.join(root, f), 0o2770)
